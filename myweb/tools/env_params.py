0cf68bfd1f75affc7916586d904bfa9b37611237
huangl08 <huangl08@mingyuanyun.com>
2021-08-13 12:02:14 +0800

    更新支持环境切换

--- myweb/tools/env_params.py
+++ myweb/tools/env_params.py
@@ -1,10 +1,12 @@
 from myweb.core.runner import CONFIG_PATH
 from myweb.utils.config import *
 
-# 获取环境变量的内容
-env_param_path = os.path.join(CONFIG_PATH, 'env_param_setting.json')
-env_param = JsonConfig(env_param_path).get()
 
-# 获取设置的环境对应的环境变量
-test_env = env_param["test_env"]
-test_param = env_param[test_env]
+# 获取环境变量的内容
+def get_env_params(file="env_param_setting.json"):
+    env_param_path = os.path.join(CONFIG_PATH, file)
+    env_param = JsonConfig(env_param_path).get()
+
+    # 获取设置的环境对应的环境变量
+    test_env = env_param["test_env"]
+    return env_param[test_env]
