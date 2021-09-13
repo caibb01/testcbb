7bc4683b9d02d3cf7ea0151e5ca66dac8b3ab458
huangl08 <huangl08@mingyuanyun.com>
2021-09-13 16:38:23 +0800

    增加获取执行环境方法

--- myweb/tools/env_params.py
+++ myweb/tools/env_params.py
@@ -10,3 +10,13 @@ def get_env_params(file="env_param_setting.json"):
     # 获取设置的环境对应的环境变量
     test_env = env_param["test_env"]
     return env_param[test_env]
+
+
+# 获取执行环境
+def get_exec_env(file="env_param_setting.json"):
+    env_param_path = os.path.join(CONFIG_PATH, file)
+    env_param = JsonConfig(env_param_path).get()
+
+    # 获取设置的环境对应的环境变量
+    test_env = env_param["test_env"]
+    return test_env
